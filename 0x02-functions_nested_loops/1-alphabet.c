762d3f27236deedb2d9aa828a7b4497a6b623704
NgachaIan <ngacha.duncan@students.jkuat.ac.ke>
2023-01-02 15:33:17 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,19 @@
 #include "main.h"
 
 /**
- *print_alphabet - check the code for ALX School students.
- *
- *Return: Always 0.
- */
+  * print_alphabet - check the code for ALX School students.
+  *
+  * Return: Always 0.
+  */
 
 void print_alphabet(void)
 
 {
-   char alp = 'a';
-   while (alp <= 'z')
-   {
-      _putchar(alp);
-      alp++;
-   }
-
-   _putchar('\n');
+char alp = 'a';
+while (alp <= 'z')
+{
+_putchar (alp);
+alp++;
+}
+_putchar ('\n');
 }
